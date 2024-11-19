4ecb9615cde6c40e7fa49e1edb826f1bec768ceb
LauraNorah <laura.nora.azevedo@gmail.com>
2024-11-19 14:34:39 -0300

    adicionado base do router seguidores

---
+++ src/routes/seguidoresRouter.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import controller from '../controller/seguidoresController.js';
+
+const router = express.Router();
+router.get('/', controller.listarSeguidores);
+
+export default router;
